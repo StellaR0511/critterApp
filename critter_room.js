be54149b737aca38e8c79298ffdde5114890d41b
StellaR0511 <81784995+StellaR0511@users.noreply.github.com>
2021-11-27 11:09:20 -0600

    Add files via upload

---
+++ critter_room.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDYqSf2W15mXc0pLTCoSUCmdWk1jMQHxKs",
+    authDomain: "crittercodeproject.firebaseapp.com",
+    projectId: "crittercodeproject",
+    storageBucket: "crittercodeproject.appspot.com",
+    messagingSenderId: "965654398887",
+    appId: "1:965654398887:web:59da6b52489770530e5882"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
